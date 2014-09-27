dd16882c46cd8f8fcabba9f1b0a9cdfdb235e714
casprian <casprian@gmail.com>
2014-09-27 13:14:15 +0530

    Update versionSpec.js

--- test/unit/versionSpec.js
+++ test/unit/versionSpec.js
@@ -5,13 +5,9 @@ describe('version', function() {
     expect(version).toEqual('0.1');
   }));
   
-   it('should return right value', inject(function(version) {
-    expect(3).toEqual(3);
+  it('should return current version', inject(function(version) {
+    expect(4).toBe(3);
   }));
-
-  it('should return wrong value', inject(function(version) {
-    expect(3).toEqual(4);
-  }));
-  
+   
   
 });
